fc1d1d75efd493c51a39d9d164434c31c57a95ce
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-04 23:10:02 -0300

    chore: add inter fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), preact()]
-});
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		preact(),
+	],
+})
